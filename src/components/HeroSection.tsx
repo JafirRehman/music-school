9268b66344deb6b671036233a026aa31ec9a23fd
JafirRehman <mjafirrehman@gmail.com>
2024-11-29 19:54:56 +0500

    fixing errors

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link"
-import { Spotlight } from "./ui/Spotlight"
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
+import React from "react";
 
 function HeroSection() {
   return (
@@ -10,26 +11,25 @@ function HeroSection() {
         fill="white"
       />
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="p-4 relative z-10 w-[90%] text-center my-5" >
-        <h1
-          className="mt-20 text-4xl notmobile:text-5xl md:text-6xl font-bold bg-clip-text text-transparent capitalize bg-gradient-to-b from-neutral-50 to-neutral-400"
-        >Master the art of music</h1>
-        <p
-          className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-        >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+      <div className="p-4 relative z-10 w-[90%] text-center my-5">
+        <h1 className="mt-20 text-4xl notmobile:text-5xl md:text-6xl font-bold bg-clip-text text-transparent capitalize bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you are a beginner or looking to refine your
+          skills, join us to unlock your true potential.
+        </p>
         <div className="mt-4">
           <Link href={"/courses"}>
-            <Button
-              borderRadius="1.7rem"
-              className="bg-black text-white ">
+            <Button borderRadius="1.7rem" className="bg-black text-white ">
               Explore Courses
             </Button>
           </Link>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
